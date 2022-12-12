ba03dcdb1ed980089ba5d2f0598d3509e5f937b0
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2022-12-12 17:48:54 +0700

    Create Banner

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Banner from '@/components/banner';
 
 export default function Home() {
+  const handleOnBannerButtonClick = () => {
+    console.log('Banner button clicked!');
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,11 +16,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Coffee Connoisseur
-        </h1>
+        <h1 className={styles.title}> Coffee Connoisseur </h1>
+        <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerButtonClick} />
       </main>
-
     </div>
   )
 }

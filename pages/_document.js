9813ab26abab4b5ed0e1151c005d8fb53576bcc3
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2022-12-12 22:41:56 +0700

    Add Font

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+    render() {
+        return <Html lang="en">
+            <Head>
+                <link rel="preload" href="/fonts/IBM_Plex_Sans/IBMPlexSans-Bold.ttf" as="font" crossOrigin="anonymous" />
+                <link rel="preload" href="/fonts/IBM_Plex_Sans/IBMPlexSans-Regular.ttf" as="font" crossOrigin="anonymous" />
+                <link rel="preload" href="/fonts/IBM_Plex_Sans/IBMPlexSans-SemiBold.ttf" as="font" crossOrigin="anonymous" />
+            </Head>
+            <body>
+                <Main></Main>
+                <NextScript />
+            </body>
+        </Html>
+    }
+} 
+
+export default MyDocument;

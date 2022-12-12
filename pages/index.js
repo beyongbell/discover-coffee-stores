46b04ef521ce74ca516259c840c29ab3addf69a2
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2022-12-13 01:01:05 +0700

    Add Data Json

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import styles from '../styles/Home.module.css'
 import Banner from '@/components/banner';
 import Card from '@/components/card';
 
+import coffeeStores from "../data/coffee-stores.json";
+
 export default function Home() {
   const handleOnBannerButtonClick = () => {
     console.log('Banner button clicked!');
@@ -22,12 +24,16 @@ export default function Home() {
           <Image src="/static/hero-image.png" width={700} height={400} alt="Hero Image" />
         </div>
         <div className={styles.cardLayout}>
-          <Card 
-            name="DarkHorse Coffee"
-            imgUrl="/static/hero-image.png" 
-            href="/coffee-store/darkhorse-coffee" 
-            className={styles.card}
-          />
+          {coffeeStores.map((coffeeStore) => {
+            return (
+              <Card
+                name={coffeeStore.name}
+                imgUrl="/static/hero-image.png"
+                href="/coffee-store/darkhose-coffee"
+                className={styles.card}
+              />
+            );
+          })}
         </div>
       </main>
     </div>

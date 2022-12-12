8ead2b110f0089d1e66518bdc7e7af30fdbb08e8
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2022-12-13 00:56:42 +0700

    Add Css Home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+
 import Banner from '@/components/banner';
+import Card from '@/components/card';
 
 export default function Home() {
   const handleOnBannerButtonClick = () => {
@@ -17,7 +19,15 @@ export default function Home() {
       <main className={styles.main}>
         <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerButtonClick} />
         <div className={styles.heroImage}>
-          <Image src="/static/hero-image.png" width={700} height={400} />
+          <Image src="/static/hero-image.png" width={700} height={400} alt="Hero Image" />
+        </div>
+        <div className={styles.cardLayout}>
+          <Card 
+            name="DarkHorse Coffee"
+            imgUrl="/static/hero-image.png" 
+            href="/coffee-store/darkhorse-coffee" 
+            className={styles.card}
+          />
         </div>
       </main>
     </div>
